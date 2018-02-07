05a8eeee9948335d2265f47a009ea8e08f8ca83c
Ruslan <dot.net.ooo@gmail.com>
2018-02-07 13:36:26 -0700

    added odata project

--- odata-research/Metadata/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Metadata
-{
-    public class Class1
-    {
-    }
-}
---
+++ odata-research/OdataSqlExtension/OdataExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OData.UriParser;
+
+namespace Helpers
+{
+    public static class OdataExtensions
+    {
+        public static string ToSqlOperator(this BinaryOperatorKind binaryOpertor)
+        {
+            switch (binaryOpertor)
+            {
+                case BinaryOperatorKind.Add:
+                    return "+";
+                case BinaryOperatorKind.And:
+                    return "AND";
+                case BinaryOperatorKind.Divide:
+                    return "/";
+                case BinaryOperatorKind.Equal:
+                    return "=";
+                case BinaryOperatorKind.GreaterThan:
+                    return ">";
+                case BinaryOperatorKind.GreaterThanOrEqual:
+                    return ">=";
+                case BinaryOperatorKind.LessThan:
+                    return "<";
+                case BinaryOperatorKind.LessThanOrEqual:
+                    return "<=";
+                case BinaryOperatorKind.Modulo:
+                    return "%";
+                case BinaryOperatorKind.Multiply:
+                    return "*";
+                case BinaryOperatorKind.NotEqual:
+                    return "!=";
+                case BinaryOperatorKind.Or:
+                    return "OR";
+                case BinaryOperatorKind.Subtract:
+                    return "-";
+                default:
+                    return null;
+            }
+        }
+    }
+}
+

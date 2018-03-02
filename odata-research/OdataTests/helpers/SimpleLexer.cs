204f58b81fd59617febfbd5712bdf4779da5b967
dot.net.ooo@gmail.com <dot.net.ooo@gmail.com>
2018-03-01 23:16:29 -0700

    started nodes

--- odata-research/OdataTests/helpers/SimpleLexer.cs
+++ odata-research/OdataTests/helpers/SimpleLexer.cs
@@ -12,7 +12,11 @@ namespace OdataTests.helpers
         int TextLen;
         int TextPos = 0;
         char? Ch;
-        string Token;
+        Node currentNode;
+        string NodeText;
+        const char QUERYOPTIONIDENTIFIER = '@';
+        const char DELIMITER = '&';
+        const char SEPARATOR = ',';
         StringBuilder _tokenizedFilter = new StringBuilder();
         TokenType SqlTokenType = TokenType.LEFTOPER;
         private static char[] Operators = new char[] { '=', '>', '<', '!' };// to be growing
@@ -33,7 +37,7 @@ namespace OdataTests.helpers
         {
             get { return _tokenizedFilter.ToString(); }
         }
-        internal string NextToken()
+        internal Node NextNode()
         {
             if (this.TextLen == this.TextPos) return null;
             ParseWhiteSpace();
@@ -42,6 +46,11 @@ namespace OdataTests.helpers
                           TokenType.LEFTOPER;
             switch (this.Ch)
             {
+                case SEPARATOR:
+                    break;
+                case QUERYOPTIONIDENTIFIER:
+                    //1. parserwhitespacee 2.read token; 
+                    break;
                 case '=':
                     SqlTokenType = TokenType.COMPARISON;
                     ReadOperator();
@@ -63,27 +72,14 @@ namespace OdataTests.helpers
                     ReadToken();
                     break;
             }
-            this.Token = this.Text.Substring(tokenPos, this.TextPos - tokenPos);
-            if (LogicalOperators.FirstOrDefault((c) => string.Equals(c, this.Token?.Trim(),
-                                 StringComparison.OrdinalIgnoreCase)) != null)
-            {
-                SqlTokenType = TokenType.LOGICAL;
-            }
-            WriteToken(this.Token);
-            return this.Token;
+            this.NodeText = this.Text.Substring(tokenPos, this.TextPos - tokenPos);
+            return this.currentNode;
         }
-        public IEnumerable<string> GetColumnToken()
+        public IEnumerable<Node> GetToken()
         {
-            while (null != (this.Token = this.NextToken()))
+            while (null != (this.currentNode = this.NextNode()))
             {
-                if (this.SqlTokenType == TokenType.LEFTOPER)
-                {
-                    yield return this.Token;
-                }
-                else
-                {
-                    continue;
-                }
+               yield  return this.currentNode;
             }
         }
         private bool IsWhiteSpace
@@ -167,29 +163,80 @@ namespace OdataTests.helpers
             }
         }
     }
+
+
+    #region Tokens
+    public abstract class Node : IQueryNode
+    {
+        protected readonly NodeType _nodeType;
+        protected readonly List<Node> _innerNodes;
+        protected readonly string _text;
+
+        protected Node(string text, NodeType nodeType, params Node[] node)
+        {
+            _innerNodes = node.ToList();
+            _text = text;
+            _nodeType = nodeType;
+        }
+       
+        public NodeType RocketNodeType
+        {
+            get { return _nodeType; }
+        }
+        public enum NodeType
+        {
+            UNDEFINED=0,
+            SELECT=1,
+            FROM=2,
+            JOIN=3,
+            WHERE=4,
+            ALIAS=5,
+            ORDERBY=6,
+            SELECTSTATEMENT=7
+        }
+        public abstract void Accept<T>(IRocketVisitor<T> visitor) where T : class;
+        public virtual List<Node> Nodes => _innerNodes?.OrderBy(n => n._nodeType).ToList();
+        public string Text => _text;
+    }
+    public interface IRocketVisitor<T> where T:class
+    {
+        T Visit(Node token);
+    }
+    public interface IQueryNode
+    {
+        void Accept<T>(IRocketVisitor<T> visitor) where T : class;
+        List<Node> Nodes { get; }
+        string Text { get; }
+    }
+    public sealed class WhereNode:Node
+    {
+        public WhereNode(string text, params Node[] nodes) : base(text, NodeType.WHERE, nodes) { }
+        
+        public override void Accept<T>(IRocketVisitor<T> visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+    public sealed class SelectStatementNode:Node
+    {
+        public SelectStatementNode(string text, params Node[] nodes) : base(text, NodeType.SELECTSTATEMENT, nodes) { }
+
+        public override void Accept<T>(IRocketVisitor<T> visitor)
+        {
+            visitor.Visit(this);
+        }
+        public override List<Node> Nodes
+        {
+            get { return base.Nodes; }
+        }
+      
+    }
+        
+#endregion
     [TestClass]
     public class SimpleLexerTest
     {
-        [TestMethod]
-        public void runToken()
-        {
-            SimpleLexer lexer = new SimpleLexer("column1=value1 AND column2=value2");
-            string token;
-            while ((token = lexer.NextToken()) != null)
-            {
-                Console.WriteLine(token);
-            }
-            
-        }
-        [TestMethod]
-        public void runAllTokens()
-        {
-            SimpleLexer lexer = new SimpleLexer("column1=value1 AND column2=value2 or column3='ruslan'");
-            lexer.GetColumnToken().ToList().ForEach((c) =>
-            {
-                Console.WriteLine(c);
-            }
-            );
-        }
+   
+      
     }
 }

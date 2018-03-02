bbcb358bc41888360319a55c12d3e4b61bb202a0
dot.net.ooo@gmail.com <dot.net.ooo@gmail.com>
2018-03-01 20:45:49 -0700

    fixed filter

--- odata-research/OdataTests/helpers/SimpleLexer.cs
+++ odata-research/OdataTests/helpers/SimpleLexer.cs
@@ -2,38 +2,87 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+
 namespace OdataTests.helpers
 {
-    public sealed  class SimpleLexer
+    public sealed class SimpleLexer
     {
         string Text;
         int TextLen;
         int TextPos = 0;
         char? Ch;
         string Token;
+        StringBuilder _tokenizedFilter = new StringBuilder();
+        TokenType SqlTokenType = TokenType.LEFTOPER;
+        private static char[] Operators = new char[] { '=', '>', '<', '!' };// to be growing
+        private static string[] ComparisonOperators = new string[] { "=", "!=", ">=", "<=", ">", "<" };
+        private static string[] LogicalOperators = new string[] { "AND", "OR", "NOT" };//to be growing
         public SimpleLexer(string filter)
         {
+            if (string.IsNullOrEmpty(filter))
+                throw new ArgumentException($"Invalid filter {filter}.");
+
             this.Text = filter;
             this.TextLen = filter.Length;
             this.TextPos = 0;
+            this.Ch = this.Text[this.TextPos];
         }
-        
-        internal  string NextToken()
+
+        internal string TokenizedSql
+        {
+            get { return _tokenizedFilter.ToString(); }
+        }
+        internal string NextToken()
         {
             if (this.TextLen == this.TextPos) return null;
-            int tokenPos = this.TextPos;
             ParseWhiteSpace();
-            ReadToken();
+            int tokenPos = this.TextPos;
+            SqlTokenType = (SqlTokenType == TokenType.COMPARISON) ? TokenType.RIGHTOPER :
+                          TokenType.LEFTOPER;
+            switch (this.Ch)
+            {
+                case '=':
+                    SqlTokenType = TokenType.COMPARISON;
+                    ReadOperator();
+                    break;
+                case '!':
+                    SqlTokenType = TokenType.COMPARISON;
+                    ReadOperator();
+                    break;
+                case '>':
+                    SqlTokenType = TokenType.COMPARISON;
+                    ReadOperator();
+                    break;
+                case '<':
+                    SqlTokenType = TokenType.COMPARISON;
+                    ReadOperator();
+                    break;
+                default:
+                    ParseWhiteSpace();
+                    ReadToken();
+                    break;
+            }
             this.Token = this.Text.Substring(tokenPos, this.TextPos - tokenPos);
+            if (LogicalOperators.FirstOrDefault((c) => string.Equals(c, this.Token?.Trim(),
+                                 StringComparison.OrdinalIgnoreCase)) != null)
+            {
+                SqlTokenType = TokenType.LOGICAL;
+            }
+            WriteToken(this.Token);
             return this.Token;
         }
-        public IEnumerable<string>GetColumnToken()
+        public IEnumerable<string> GetColumnToken()
         {
-            while (null!=(this.Token = this.NextToken()))
+            while (null != (this.Token = this.NextToken()))
             {
-                if (this.Token.Contains('='))
+                if (this.SqlTokenType == TokenType.LEFTOPER)
                 {
-                    yield return this.Token.Split(new char[] { '=' })?.FirstOrDefault();
+                    yield return this.Token;
+                }
+                else
+                {
+                    continue;
                 }
             }
         }
@@ -46,7 +95,7 @@ namespace OdataTests.helpers
         }
         private void ParseWhiteSpace()
         {
-            while(IsWhiteSpace)
+            while (IsWhiteSpace)
             {
                 this.NextChar();
             }
@@ -54,7 +103,22 @@ namespace OdataTests.helpers
         private void ReadToken()
         {
             if (!Ch.HasValue) NextChar();
-            while(!IsWhiteSpace && this.Ch.HasValue)
+            while (this.Ch.HasValue && !IsWhiteSpace && !Operators.Contains(this.Ch.Value))
+            {
+                NextChar();
+            }
+        }
+        private void ReadLiteral()
+        {
+            if (!Ch.HasValue) NextChar();
+            while (this.Ch.HasValue && !IsWhiteSpace)
+            {
+                NextChar();
+            }
+        }
+        private void ReadOperator()
+        {
+            while (this.Ch.HasValue && Operators.Contains(this.Ch.Value))
             {
                 NextChar();
             }
@@ -69,9 +133,38 @@ namespace OdataTests.helpers
                     this.Ch = this.Text[this.TextPos];
                     return;
                 }
-               
+
             }
             this.Ch = null;
+        }
+        internal enum TokenType
+        {
+            UNDEFINED = 0,
+            COMPARISON = 1,
+            LOGICAL = 2,
+            LEFTOPER = 3,
+            RIGHTOPER = 4,
+            END = 5
+        }
+
+        private void WriteToken(string token)
+        {
+            switch (this.SqlTokenType)
+            {
+                case TokenType.COMPARISON:
+                    _tokenizedFilter.AppendFormat("{0}", token);
+                    break;
+                case TokenType.LEFTOPER:
+                    _tokenizedFilter.AppendFormat(" @{0}", token);
+                    break;
+                case TokenType.LOGICAL:
+                    _tokenizedFilter.AppendFormat(" {0} ", token);
+                    break;
+                case TokenType.RIGHTOPER:
+                    _tokenizedFilter.AppendFormat("{0} ", token);
+                    break;
+
+            }
         }
     }
     [TestClass]

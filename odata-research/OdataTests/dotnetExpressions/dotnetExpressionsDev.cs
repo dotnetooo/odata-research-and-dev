4f1a10262f35ba317df1a40c3f34561473321ee3
Ruslan <dot.net.ooo@gmail.com>
2018-03-07 14:10:58 -0700

    added expression building

---
+++ odata-research/OdataTests/dotnetExpressions/dotnetExpressionsDev.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace OdataTests.dotnetExpressions
+{
+    [TestClass]
+   public  class dotnetExpressionsDev
+    {
+        [TestMethod]
+        public void CheckBinaryExpression()
+        {
+            var exp = ExpressionExtensions.GetExpression<int>(5, 10);
+            var value = exp.ToString();
+        }
+        [TestMethod]
+        public void GetLambdaExpression()
+        {
+           var lexp = ExpressionExtensions.GetLambdaExp("ok", "ok");
+            Func<string, string, bool> input = (Func<string, string, bool>)lexp.Compile();
+            Expression<Func<string, string, bool>> expression = (arg1, arg2) => input(arg1, arg2);
+            var result=  ExpressionExtensions.Invoke(expression);
+            ExpressionExtensions.Invoke((arg1, arg2) => input(arg1, arg2));
+        }
+    }
+        
+    public static class ExpressionExtensions
+    {
+        public static BinaryExpression GetExpression<T>(int operLeft,int operRight)
+        {
+            return Expression<T>.MakeBinary(ExpressionType.Add,
+                Expression.Constant(operLeft),
+                Expression.Constant(operRight)
+                );
+        }
+        public static LambdaExpression GetLambdaExp(string left, string right)
+        {
+            ParameterExpression paramExpr = Expression.Parameter(typeof(string), "arg");
+            ParameterExpression paramExpr2 = Expression.Parameter(typeof(string), "arg2");
+            return Expression.Lambda<Func<string, string, bool>>(
+                Expression.MakeBinary(ExpressionType.Equal,
+                                      paramExpr,
+                                      paramExpr2),
+                                      paramExpr, paramExpr2);
+                
+        }
+       
+        public static LambdaExpression GetLambdaExp()
+        {
+            return Expression.Lambda(
+                GetExpression<int>(10, 50));        }
+
+        public static bool Invoke(Expression<Func<string,string,bool>> expression)
+        {
+            return expression.Compile().Invoke("ok", "ok");
+        }
+    }
+}
